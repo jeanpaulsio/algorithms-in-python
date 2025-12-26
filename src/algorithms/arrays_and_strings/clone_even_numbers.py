38f7102a0c5eb2ba205351aefce19ae011d8d1eb
jeanpaulsio <hello@jpsio.me>
2025-12-26 14:12:54 -0800

    Add docstrings

--- src/algorithms/arrays_and_strings/clone_even_numbers.py
+++ src/algorithms/arrays_and_strings/clone_even_numbers.py
@@ -1,4 +1,15 @@
 def clone_even_numbers(arr):
+    """
+    Replace each even number with two of the same number.
+
+    Given an array of numbers, replace each even number with two of the same number.
+    Assume that the array has the exact number of space to accommodate the new numbers.
+    Empty slots are represented by -1.
+
+    Examples:
+        >>> clone_even_numbers([2, 4, 6, -1, -1, -1])
+        [2, 2, 4, 4, 6, 6]
+    """
     if not arr:
         return arr
 
--- src/algorithms/arrays_and_strings/reverse_words.py
+++ src/algorithms/arrays_and_strings/reverse_words.py
@@ -1,4 +1,20 @@
 def reverse_words(s):
+    """
+    Reverse the order of words in a string.
+
+    Given a string, return a new string with the words in reverse order.
+    Words are separated by spaces. Leading and trailing spaces are ignored.
+
+    Examples:
+        >>> reverse_words("foo bar")
+        'bar foo'
+        >>> reverse_words("foo bar baz quux")
+        'quux baz bar foo'
+        >>> reverse_words(" foo")
+        'foo'
+        >>> reverse_words("foo ")
+        'foo'
+    """
     result = ""
     i = len(s) - 1
     j = len(s)
